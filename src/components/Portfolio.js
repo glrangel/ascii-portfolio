c5fc4aebf873a98e8a58d4ab0e276e78ddbdb0a1
Gabriel <gabriel@macbookpro.lan>
2025-12-06 15:27:54 -0800

    Update projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,8 +7,8 @@ function Portfolio () {
     const isDesktop = useMediaQuery({ query: '(min-width: 1000px)' });
 
     return (
-        // <div id= "portfolio-wrapper">
-            <motion.div id="portfolio" class={!isDesktop && "overflow-main"}
+        <div id= "portfolio-wrapper">
+            <motion.div id="portfolio" class={!isDesktop ? "overflow-main" : "mobile-portfolio"}
                 initial={{y: 5, opacity: 0}}
                 animate={{y: 0, opacity: 1}}
                 transition={{duration: 2, type: 'spring', stiffness: 30}}
@@ -23,17 +23,23 @@ function Portfolio () {
                     title={'Responsive Coding Challenge'}
                     subtitle={'fully responsive pure html/css'} 
                     link={'https://competent-dijkstra-7a8fca.netlify.app/'}
-                    width={'50vw'}
+                    // width={'50vw'}
                 />
                 <Project
                     title={'Witches And Science'}
                     subtitle={'Interactive landing page made using HTML/CSS/JavsScript'} 
                     link={'https://www.witchesandscience.com/'}
-                    width={'65vw'}
                 />
+                <Project
+                    title={'Portfolio Website'}
+                    subtitle={'3D Rendered Portfolio made using React/ThreeJS'} 
+                    link={'https://gabrielrangel.dev'}
+                    // width={'50vw'}
+                />
+                
 
             </motion.div>
-        // </div>
+        </div>
     )
 }
 
